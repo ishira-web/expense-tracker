bd10e1936870215aa8d81513884af520f2a5e948
Ishira Pahasara <st20281934@outlook.cardiffmet>
2026-01-22 11:22:06 +0530

    update port

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import userRoutes from './route/user.routes';
 dotenv.config();
 
 const app = express();
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = process.env.PORT
 
 // CORS configuration - Allow all in production or specific locally
 app.use(cors({
